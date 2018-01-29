fbfc7c9d2ba46bbc89a7c86d64623425c13da31c
Karnblad <OKARNBLA@volvocars.com>
2018-01-29 16:29:08 +0100

    Model with checkpoint, etc. added

--- models/gru_baseline/gru_main.py
+++ models/gru_baseline/gru_main.py
@@ -2,12 +2,14 @@ import sys, os, re, csv, codecs, numpy as np, pandas as pd
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, Input, GRU, Embedding, Dropout, Activation
-from keras.layers import Bidirectional, GlobalMaxPool1D, BatchNormalization
+from keras.layers import Dense, Input, LSTM, GRU, Embedding, Dropout, Activation, BatchNormalization
+from keras.layers import Bidirectional, GlobalMaxPool1D
 from keras.models import Model
-from keras import initializers, regularizers, constraints, optimizers, layers
+from keras.callbacks import ModelCheckpoint
 
 path = '../../input/'
+EMBEDDING_FILE=f'{path}glove.6B.50d.txt'
+
 TRAIN_DATA_FILE=f'{path}train.csv'
 TEST_DATA_FILE=f'{path}test.csv'
 
@@ -17,6 +19,17 @@ maxlen = 100 # max number of words in a comment to use
 
 print('Load train data')
 train = pd.read_csv(TRAIN_DATA_FILE)
+
+print('Removing special characters')
+for i, text in enumerate(train['comment_text']):
+    print('Cleaning {} of {}'.format(i, len(train)))
+    clean_text = re.sub('[^A-Za-z]+', ' ', text)
+    train['comment_text'].iloc[i] = clean_text
+
+train.to_csv(path_or_buf=f'{path}train_clean.csv')
+print('Cleaned train saved')
+
+print(train['comment_text'].head(3))
 
 print('Load test data')
 test = pd.read_csv(TEST_DATA_FILE)
@@ -30,33 +43,42 @@ list_sentences_test = test["comment_text"].fillna("_na_").values
 print('Tokenizing')
 tokenizer = Tokenizer(num_words=max_features)
 tokenizer.fit_on_texts(list(list_sentences_train))
-
 list_tokenized_train = tokenizer.texts_to_sequences(list_sentences_train)
 list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
-
-print('Padding')
 X_t = pad_sequences(list_tokenized_train, maxlen=maxlen)
 X_te = pad_sequences(list_tokenized_test, maxlen=maxlen)
 
+def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+embeddings_index = dict(get_coefs(*o.strip().split()) for o in open(EMBEDDING_FILE, encoding="utf8"))
+
+all_embs = np.stack(embeddings_index.values())
+emb_mean,emb_std = all_embs.mean(), all_embs.std()
+
+word_index = tokenizer.word_index
+nb_words = min(max_features, len(word_index))
+embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+for word, i in word_index.items():
+    if i >= max_features: continue
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
 print('Retrieving model')
 inp = Input(shape=(maxlen,))
-x = Embedding(max_features, embed_size)(inp)
-x = Bidirectional(GRU(32, return_sequences=True, dropout=0.1, recurrent_dropout=0.2))(x)
-x = BatchNormalization(momentum=0.99, epsilon=0.001)(x)
+x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+x = Bidirectional(LSTM(50, return_sequences=True, dropout=0.2, recurrent_dropout=0.1))(x)
 x = GlobalMaxPool1D()(x)
-x = Dense(32, activation="relu")(x)
-x = Dropout(0.2)(x)
-x = Dense(6, activation="softmax")(x)
+x = Dense(50, activation="relu")(x)
+x = Dropout(0.1)(x)
+x = Dense(6, activation="sigmoid")(x)
 model = Model(inputs=inp, outputs=x)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 print('Training')
-model.fit(X_t, y, batch_size=32, epochs=2)
+mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='auto')
+model.fit(X_t, y, batch_size=32, epochs=2, validation_split=0.1, callbacks=[mcp_save])
 
 print('Predicting')
 y_test = model.predict([X_te], batch_size=1024, verbose=1)
-
-print('Retrieving sample submissions')
 sample_submission = pd.read_csv(f'{path}sample_submission.csv')
 sample_submission[list_classes] = y_test
-sample_submission.to_csv('../../submissions/gru_maxlen150_lb.csv', index=False)
+sample_submission.to_csv('C:/Users/okarnbla/Documents/private_repos/Kaggle/toxic_comment/submissions/LSTM_lrdecr_NoGlove_lb.csv', index=False)
