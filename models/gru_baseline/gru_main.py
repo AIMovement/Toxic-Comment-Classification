43480a26cea03ae6d64abe349a221e972eed043f
Karnblad <OKARNBLA@volvocars.com>
2018-03-01 15:26:23 +0100

    Dummy commit

--- models/gru_baseline/gru_main.py
+++ models/gru_baseline/gru_main.py
@@ -5,7 +5,9 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense, Input, LSTM, GRU, Embedding, Dropout, Activation, BatchNormalization
 from keras.layers import Bidirectional, GlobalMaxPool1D
 from keras.models import Model
+from keras.regularizers import l1_l2
 from keras.callbacks import ModelCheckpoint
+from sklearn.metrics import fbeta_score
 
 path = '../../input/'
 EMBEDDING_FILE=f'{path}glove.6B.50d.txt'
@@ -20,17 +22,8 @@ maxlen = 100 # max number of words in a comment to use
 print('Load train data')
 train = pd.read_csv(TRAIN_DATA_FILE)
 
-print('Removing special characters')
-for i, text in enumerate(train['comment_text']):
-    print('Cleaning {} of {}'.format(i, len(train)))
-    clean_text = re.sub('[^A-Za-z]+', ' ', text)
-    train['comment_text'].iloc[i] = clean_text
-
-train.to_csv(path_or_buf=f'{path}train_clean.csv')
-print('Cleaned train saved')
-
 print(train['comment_text'].head(3))
-
+    
 print('Load test data')
 test = pd.read_csv(TEST_DATA_FILE)
 
@@ -65,9 +58,9 @@ for word, i in word_index.items():
 print('Retrieving model')
 inp = Input(shape=(maxlen,))
 x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
-x = Bidirectional(LSTM(50, return_sequences=True, dropout=0.2, recurrent_dropout=0.1))(x)
+x = Bidirectional(LSTM(64, return_sequences=True, dropout=0.2, recurrent_dropout=0.1, kernel_regularizer=l1_l2(0.001, 0.01)))(x)
 x = GlobalMaxPool1D()(x)
-x = Dense(50, activation="relu")(x)
+x = Dense(64, activation="relu")(x)
 x = Dropout(0.1)(x)
 x = Dense(6, activation="sigmoid")(x)
 model = Model(inputs=inp, outputs=x)
@@ -78,7 +71,8 @@ mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_lo
 model.fit(X_t, y, batch_size=32, epochs=2, validation_split=0.1, callbacks=[mcp_save])
 
 print('Predicting')
+model.load_weights('.mdl_wts.hdf5')
 y_test = model.predict([X_te], batch_size=1024, verbose=1)
 sample_submission = pd.read_csv(f'{path}sample_submission.csv')
 sample_submission[list_classes] = y_test
-sample_submission.to_csv('C:/Users/okarnbla/Documents/private_repos/Kaggle/toxic_comment/submissions/LSTM_lrdecr_NoGlove_lb.csv', index=False)
+sample_submission.to_csv('C:/Users/okarnbla/Documents/private_repos/Kaggle/toxic_comment/submissions/LSTM_regularization64neuron_roc.csv', index=False)
--- models/gru_multi-layer/gru_main.py
+++ models/gru_multi-layer/gru_main.py
@@ -2,15 +2,17 @@ import sys, os, re, csv, codecs, numpy as np, pandas as pd
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, Input, GRU, Embedding, Dropout, Activation
+from keras.layers import Dense, Input, GRU, Embedding, Dropout, Activation, LSTM
 from keras.layers.merge import add, concatenate
 from keras.layers import Bidirectional, GlobalMaxPool1D, BatchNormalization
 from keras.models import Model, Sequential
 from keras import initializers, regularizers, constraints, optimizers, layers
+from keras.callbacks import ModelCheckpoint
 
 path = '../../input/'
 TRAIN_DATA_FILE=f'{path}train.csv'
 TEST_DATA_FILE=f'{path}test.csv'
+EMBEDDING_FILE=f'{path}glove.6B.50d.txt'
 
 embed_size = 50 # how big is each word vector
 max_features = 20000 # how many unique words to use (i.e num rows in embedding vector)
@@ -39,20 +41,44 @@ print('Padding')
 X_t = pad_sequences(list_tokenized_train, maxlen=maxlen)
 X_te = pad_sequences(list_tokenized_test, maxlen=maxlen)
 
+def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+embeddings_index = dict(get_coefs(*o.strip().split()) for o in open(EMBEDDING_FILE, encoding="utf8"))
+
+all_embs = np.stack(embeddings_index.values())
+emb_mean,emb_std = all_embs.mean(), all_embs.std()
+
+word_index = tokenizer.word_index
+nb_words = min(max_features, len(word_index))
+embedding_matrix = np.random.normal(emb_mean, emb_std, (nb_words, embed_size))
+for word, i in word_index.items():
+    if i >= max_features: continue
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+
 print('Retrieving model')
 inp = Input(shape=(maxlen,))
 
-left = Embedding(max_features, embed_size)(inp)
-left = Bidirectional(GRU(16, return_sequences=True, dropout=0.1, recurrent_dropout=0.2, kernel_regularizer=regularizers.l1(0.0001)))(left)
-left = BatchNormalization(momentum=0.99, epsilon=0.001)(left)
-left = GlobalMaxPool1D()(left)
+one = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+one = Bidirectional(LSTM(8, return_sequences=True, dropout=0.1, recurrent_dropout=0.1, kernel_regularizer=regularizers.l1(0.0001)))(one)
+one = BatchNormalization(momentum=0.99, epsilon=0.001)(one)
+one = GlobalMaxPool1D()(one)
 
-right = Embedding(max_features, embed_size)(inp)
-right = Bidirectional(GRU(16, return_sequences=True, dropout=0.2, recurrent_dropout=0.1, kernel_regularizer=regularizers.l1(0.0001)))(right)
-right = BatchNormalization(momentum=0.99, epsilon=0.001)(right)
-right = GlobalMaxPool1D()(right)
+two = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+two = Bidirectional(GRU(8, return_sequences=True, dropout=0.1, recurrent_dropout=0.1, kernel_regularizer=regularizers.l1(0.0001)))(two)
+two = BatchNormalization(momentum=0.99, epsilon=0.001)(two)
+two = GlobalMaxPool1D()(two)
 
-x = concatenate([left, right])
+#three = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+#three = Bidirectional(LSTM(4, return_sequences=True, dropout=0.1, recurrent_dropout=0.1, kernel_regularizer=regularizers.l1(0.0001)))(three)
+#three = BatchNormalization(momentum=0.99, epsilon=0.001)(three)
+#three = GlobalMaxPool1D()(three)
+
+#four = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+#four = Bidirectional(GRU(2, return_sequences=True, dropout=0.1, recurrent_dropout=0.1, kernel_regularizer=regularizers.l1(0.0001)))(four)
+#four = BatchNormalization(momentum=0.99, epsilon=0.001)(four)
+#four = GlobalMaxPool1D()(four)
+
+x = concatenate([one, two])
 
 x = Dense(16, activation="relu")(x)
 x = Dropout(0.2)(x)
@@ -62,6 +88,7 @@ model = Model(inputs=inp, outputs=x)
 model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
 print('Training')
+mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='auto')
 model.fit(X_t, y, batch_size=16, epochs=2)
 
 print('Predicting')
@@ -70,4 +97,4 @@ y_test = model.predict([X_te], batch_size=1024, verbose=1)
 print('Retrieving sample submissions')
 sample_submission = pd.read_csv(f'{path}sample_submission.csv')
 sample_submission[list_classes] = y_test
-sample_submission.to_csv('../../submissions/gru_multi3_batchnorm.csv', index=False)
+sample_submission.to_csv('../../submissions/gru_2layer_embedding_multi_roc.csv', index=False)
